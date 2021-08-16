36a922eada48382f94e4eabc48685b8cdbabed4b
lyjustinn <87103438+lyjustinn@users.noreply.github.com>
2021-08-15 23:40:41 -0400

    modify port for the server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require("./app");
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
+let PORT = process.env.PORT || 3000;
+let PORT = process.env.NODE_ENV === "production" ? 80 : PORT;
 
 app.listen(PORT, () => {
     console.log('Server running on port ' + PORT);
